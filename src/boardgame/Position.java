28ff8e098a342795b477b0699a8e5ae7881b2fce
tiagocabralk <tiagocabralk@outlook.com>
2022-10-02 21:04:17 -0300

    Criado a classe Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position() {
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setValue(int row, int column) {
+
+    }
+
+    @Override
+    public String toString() {
+        return row + " | " + column;
+    }
+}
